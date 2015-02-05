dc81d7d2536fdda81ebf3548c1bacf421ffbd835
sinharakesh <rksinha.btech@gmail.com>
2015-02-05 14:12:47 +0530

    draggable tool start

--- templates/js/editortool.js
+++ templates/js/editortool.js
@@ -2,6 +2,9 @@ jQuery.widget("ui.editortool",{
 	
 	_create: function(){
 		$(this.element).css('border','2px solid orange');
+		$(this.element).draggable({
+			helper: 'clone'
+		});
 	}
 	
 });
--- templates/js/entityinstance.js
+++ templates/js/entityinstance.js
@@ -1,9 +1,20 @@
 jQuery.widget("ui.entityinstance",{
 	
 	_create: function(){
+		var self = this;
 		$(this.element).css('border','2px solid yellow');
+		$(this.element).css('width','20%');
+		$(this.element).css('height','20%');
+
 		$(this.element).draggable({
-			helper: 'clone'
+			helper: 'clone',
+			containment: "parent"
+		}).droppable({
+			drop: function(event,ui){
+				console.log(event);
+				$('<div>').html($(ui.helper.context.outerHTML).html()).appendTo(self.element).entityinstance();
+				event.stopPropagation();
+			}
 		});
 	}
 	
