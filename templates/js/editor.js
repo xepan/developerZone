5ac3f1ab76390f108ebfa8b2a21af5a562fd91b4
sinharakesh <rksinha.btech@gmail.com>
2015-02-05 16:00:23 +0530

    editor started

--- templates/js/editor.js
+++ templates/js/editor.js
@@ -2,6 +2,7 @@ jQuery.widget("ui.editor",{
 	
 	options:{
 		logic:{},
+		includePlugins:['Model']
 	},
 
 	_create: function(){
@@ -9,6 +10,42 @@ jQuery.widget("ui.editor",{
 		self.setupEditor();
 		self.loadDesign();
 		self.render();
+	},
+
+	setupEditor: function(){
+		var self = this;
+		// Setup Plugins
+		self.loadPlugins();
+		//Make Editor droppable
+		$(self.element).css('border','2px solid blue');
+		$(self.element).droppable({
+			
+			drop: function(event,ui){
+				if(!ui.draggable.hasClass('newInstance')){
+					// console.log(ui.draggable.data("type"));
+					//Update Logic
+					$('<div class="newInstance">').html($(ui.helper.context.outerHTML).html()).appendTo(self.element);
+				}
+
+			}
+
+		});
+	},
+
+	loadPlugins: function(){
+		var self = this;
+		$.each(this.options.includePlugins, function(index, js_file) {
+			$.atk4.includeJS("epan-components/developerZone/templates/js/plugins/"+js_file+".js");
+		});
+	},
+
+
+	loadDesign: function(){
+
+	},
+
+	render: function(){
+
 	}
 	
 });
---
+++ templates/js/plugins/Model.js
@@ -0,0 +1,16 @@
+Model = function (params){
+	this.editor= undefined;
+	
+	this.options = {
+		
+	};
+
+	this.init = function(){
+		var self = this;
+		
+	}
+
+	this.render = function(){
+		var self = this;
+	}
+}
