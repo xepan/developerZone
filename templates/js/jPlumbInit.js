8020b16e01f0c8060eea28af0a01dbaac898a551
sinharakesh <rksinha.btech@gmail.com>
2015-02-06 10:16:47 +0530

    connection removed done

--- templates/js/jPlumbInit.js
+++ templates/js/jPlumbInit.js
@@ -1,4 +1,21 @@
-  jsPlumb.Defaults.Container= $("#web_layout_fluid_view");
-  jsPlumb.Defaults.PaintStyle = { strokeStyle:"#F09E30", lineWidth:2, dashstyle: '3 3', };
-  jsPlumb.Defaults.EndpointStyle = { radius:7, fillStyle:"#F09E30" };
-  jsPlumb.importDefaults({Connector : [ "Bezier", { curviness:50 } ]});
+	jsPlumb.Defaults.Container= $("#web_layout_fluid_view");
+	jsPlumb.Defaults.PaintStyle = { strokeStyle:"#F09E30", lineWidth:2, dashstyle: '3 3', };
+	jsPlumb.Defaults.EndpointStyle = { radius:7, fillStyle:"#F09E30" };
+	jsPlumb.importDefaults({Connector : [ "Bezier", { curviness:50 } ]});
+	
+	//Connector Called
+	jsPlumb.bind("connection", function(info) {
+		editor = $('.editor-document').data('uiEditor');
+		editor.options.logic.connections.push(info);
+	});
+
+	//connections detached
+	jsPlumb.bind("connectionDetached",function(info,originalEvent){
+		editor = $('.editor-document').data('uiEditor');
+		connections = editor.options.logic.connections;
+		$.each(connections, function(index, obj) {
+			if(obj.sourceId === info.sourceId && obj.targetId === info.targetId)
+				editor.options.logic.connections.splice(index,1);
+		});
+		//Remove Detached Connection form the editor.options.connections
+	});
--- templates/js/saveCode.js
+++ templates/js/saveCode.js
@@ -1,5 +1,17 @@
 $.each({
 	saveCode : function(){
-		alert('called');
+		editor = $('.editor-document').data('uiEditor');
+		nodes = editor.options.logic.nodes;
+		connections = editor.options.logic.connections;
+		//Nodes
+		$.each(nodes, function(index,obj){
+			
+		});
+
+		//Connections
+		$.each(connections, function(index,obj){
+			
+		});
+		
 	}
 }, $.univ._import);
